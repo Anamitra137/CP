8b8ab30513954b00344b6855e529ef073d2ae4e5
anamitra137 <anamitra137@gmail.com>
2023-06-28 04:11:29 +0530

    27/06 more work

--- ContestsAndElse/CF880C.cpp
+++ ContestsAndElse/CF880C.cpp
@@ -11,7 +11,6 @@ using ii = pair<int, int>;
 
 constexpr int MOD = 1e9+7;
 
-int num[] = {0, 9, 90, 900, 9000, 90000, 900000};
 int mn[] = {0, 1, 10, 100, 1000, 10000, 100000};
 int mx[] = {0, 9, 99, 999, 9999, 99999, 999999};
 
@@ -19,15 +18,10 @@ void solve(){
     int A, B, C; cin >> A >> B >> C;
     ll k; cin >> k;
 
-    // if(max(A, B) > C || C > max(A, B) + 1){
-    //     cout << -1 << '\n';
-    //     return;
-    // }
-
     if(max(A, B) == C){
         ll cnt = 0;
         for(int a = mn[A]; a <= min(mx[A], mx[C] - mn[B]); ++a){
-            int total = min(mx[C], a + mx[B]) - (a + mn[B]) + 1;
+            int total = min(mx[C]-a, mx[B]) - mn[B] + 1;
             // cout << a << ' ' << cnt << ' ' << total << '\n';
             if(cnt + total >= k){
                 int b = mn[B] + (k-cnt) - 1;
@@ -43,10 +37,11 @@ void solve(){
     else if(max(A, B) + 1 == C){
         ll cnt = 0;
         for(int a = max(mn[A], mn[C]-mx[B]); a <= mx[A]; ++a){
-            int total = mx[B] - (mn[C]-a) + 1;
+            int bmin = max(mn[B], mn[C]-a);
+            int total = mx[B] - bmin + 1;
             // cout << a << ' ' << cnt << ' ' << total << '\n';
             if(cnt + total >= k){
-                int b = (mn[C]-a) + (k-cnt) - 1;
+                int b = bmin + (k-cnt) - 1;
                 cout << a << " + " << b << " = " << (a+b) << '\n';
                 return;
             }
